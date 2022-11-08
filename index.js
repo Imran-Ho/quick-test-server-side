927fa5ee94412089b2429a2496f5aa985f2ef625
Imran-Ho <imranhossain36@gmail.com>
2022-11-08 10:24:57 +0600

    dotenv is executed

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 // middleWare added
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gof4ucb.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res)=>{
     res.send('server is created for providing service')
