5375cef9d9eb410da6a9fb680f7b7688057de930
Imran-Ho <imranhossain36@gmail.com>
2022-11-08 10:27:02 +0600

    set up connection with DB

--- index.js
+++ index.js
@@ -16,12 +16,17 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gof4ucb.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+function run(){
+    try{
+
+    }
+    finally{
+        
+    }
+
+}
+run().catch(err => console.log(err))
 
 app.get('/', (req, res)=>{
     res.send('server is created for providing service')
