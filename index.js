b7b950d4ef7bd2bc6dec7a63dd49cf6c004189fe
Imran-Ho <imranhossain36@gmail.com>
2022-11-08 10:02:09 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const app = express()
+const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+// middleWare added
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send('server is created for providing service')
